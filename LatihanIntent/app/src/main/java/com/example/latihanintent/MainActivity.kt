382d91b628a07a041877e0c2927227d38a00e301
Juzairi Safitli <airiagustus82@gmail.com>
2023-11-25 23:58:17 +0700

    mengirim data intent, parcel, dan implicit intent

--- LatihanIntent/app/src/main/java/com/example/latihanintent/MainActivity.kt
+++ LatihanIntent/app/src/main/java/com/example/latihanintent/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.latihanintent
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -16,6 +17,12 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         val btnMoveWithData: Button = findViewById(R.id.btn_move_with_data)
         btnMoveWithData.setOnClickListener(this)
+
+        val btnMoveWithObject: Button = findViewById(R.id.btn_move_activity_object)
+        btnMoveWithObject.setOnClickListener(this)
+
+        val btnDialPhone: Button = findViewById(R.id.btn_dial_number)
+        btnDialPhone.setOnClickListener(this)
     }
 
     override fun onClick (v: View?) {
@@ -30,6 +37,23 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 5)
                 startActivity(moveWithDataIntent)
             }
+            R.id.btn_move_activity_object -> {
+                val person = Person(
+                "Juzairi Safitli",
+                16,
+                "airiagustus82@gmail.com",
+                "Jambi"
+                )
+                val moveWithObjectIntent = Intent(this@MainActivity, MoveWithObjectActivity::class.java)
+                moveWithObjectIntent.putExtra(MoveWithObjectActivity.EXTRA_PERSON, person)
+                startActivity(moveWithObjectIntent)
+            }
+            R.id.btn_dial_number -> {
+                val phoneNumber = "082186657316"
+                val dialPhoneIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phoneNumber"))
+                startActivity(dialPhoneIntent)
+            }
+
         }
     }
 
